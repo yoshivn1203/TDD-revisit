8d8f9c3dadbef094970573f5fb5b5652afd33929
yoshivn1203 <yoshivn1203@gmail.com>
2023-05-01 23:42:21 +0700

    finish section 4

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -13,35 +13,37 @@ beforeEach(() => {
 });
 
 describe('User Registration', () => {
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    });
+  const validUser = {
+    username: 'user1',
+    email: 'user1@mail.com',
+    password: 'P4ssword',
+  };
+
+  const postUser = (user = validUser) => {
+    return request(app).post('/api/1.0/users').send(user);
   };
 
   it('return 200 ok when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
 
     expect(response.status).toBe(200);
   });
 
   it('return message success when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
 
     expect(response.body.message).toBe('User created');
   });
 
   it('save the user to database', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('save the username and email to database', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     const savedUser = userList[0];
@@ -51,10 +53,144 @@ describe('User Registration', () => {
   });
 
   it('hashes te password in database', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.password).not.toBe('P4ssword');
+  });
+
+  it('return 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('return validationErrors field in response body when validation error occurs', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+
+    const { body } = response;
+
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('return errors for both when username and email is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: null,
+      password: 'P4ssword',
+    });
+
+    const { body } = response;
+
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  // it('return Username cannot be null when username is null', async () => {
+  //   const response = await postUser({
+  //     username: null,
+  //     email: 'user1@mail.com',
+  //     password: 'P4ssword',
+  //   });
+
+  //   const { body } = response;
+
+  //   expect(body.validationErrors.username).toBe('Username cannot be null');
+  // });
+
+  // it('return Email cannot be null when email is null', async () => {
+  //   const response = await postUser({
+  //     username: 'user1',
+  //     email: null,
+  //     password: 'P4ssword',
+  //   });
+
+  //   const { body } = response;
+
+  //   expect(body.validationErrors.email).toBe('Email cannot be null');
+  // });
+
+  // it('return password cannot be null message when password is null', async () => {
+  //   const response = await postUser({
+  //     username: 'user1',
+  //     email: 'user1@mail.com',
+  //     password: null,
+  //   });
+
+  //   const { body } = response;
+
+  //   expect(body.validationErrors.password).toBe('Password cannot be null');
+  // });
+
+  // it.each([
+  //   ['username', 'Username cannot be null'],
+  //   ['email', 'Email cannot be null'],
+  //   ['password', 'Password cannot be null'],
+  // ])('when %s is null %s is received', async (field, expectedMessage) => {
+  //   const user = {
+  //     username: 'user1',
+  //     email: 'user1@mail.com',
+  //     password: 'P4ssword',
+  //   };
+
+  //   user[field] = null;
+  //   const response = await postUser(user);
+  //   const { body } = response;
+  //   expect(body.validationErrors[field]).toBe(expectedMessage);
+  // });
+
+  it.each`
+    field         | value              | expectedMessage
+    ${'username'} | ${null}            | ${'Username cannot be null'}
+    ${'username'} | ${'usr'}           | ${'Must have min 4 and max 32 characters'}
+    ${'username'} | ${'a'.repeat(33)}  | ${'Must have min 4 and max 32 characters'}
+    ${'email'}    | ${null}            | ${'Email cannot be null'}
+    ${'email'}    | ${'mail.com'}      | ${'Email is not valid'}
+    ${'password'} | ${null}            | ${'Password cannot be null'}
+    ${'password'} | ${'P4ssw'}         | ${'Password must be at least 6 characters'}
+    ${'password'} | ${'alllowercase'}  | ${'Password have at least 1 uppercase, 1 lower case and 1 number'}
+    ${'password'} | ${'ALLUPPERCASE'}  | ${'Password have at least 1 uppercase, 1 lower case and 1 number'}
+    ${'password'} | ${'11112323'}      | ${'Password have at least 1 uppercase, 1 lower case and 1 number'}
+    ${'password'} | ${'lowerAndUPPER'} | ${'Password have at least 1 uppercase, 1 lower case and 1 number'}
+  `(
+    'return $expectedMessage when $field is $value',
+    async ({ field, expectedMessage, value }) => {
+      const user = {
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      };
+
+      user[field] = value;
+      const response = await postUser(user);
+      const { body } = response;
+      expect(body.validationErrors[field]).toBe(expectedMessage);
+    }
+  );
+
+  it('return Email in use when the same email is already in use', async () => {
+    await User.create({ ...validUser });
+    const response = await postUser();
+    const { body } = response;
+    expect(body.validationErrors.email).toBe('Email in use');
+  });
+
+  it('return errors for both username is null and email is already in use', async () => {
+    await User.create({ ...validUser });
+    const response = await postUser({
+      username: null,
+      email: validUser.email,
+      password: 'P4ssword',
+    });
+    const { body } = response;
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
   });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,11 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const User = require('./User');
 const UserService = require('./UserService');
+const { check, validationResult } = require('express-validator');
 
-router.post('/api/1.0/users', async (req, res) => {
-  await UserService.save(req.body);
+// VALIDATE WITHOUT USING EXPRESS VALIDATOR, PASS DATA TO REQ.BODY
 
-  return res.send({ message: 'User created' });
-});
+// const validateUsername = (req, res, next) => {
+//   const user = req.body;
+
+//   if (user.username === null) {
+//     req.validationErrors = {
+//       username: 'Username cannot be null',
+//     };
+//   }
+
+//   next();
+// };
+
+// const validateEmail = (req, res, next) => {
+//   const user = req.body;
+
+//   if (user.email === null) {
+//     req.validationErrors = {
+//       ...req.validationErrors,
+//       email: 'Email cannot be null',
+//     };
+//   }
+//   next();
+// };
+
+router.post(
+  '/api/1.0/users',
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters')
+    .bail(),
+  check('email')
+    .notEmpty()
+    .withMessage('Email cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not valid')
+    .bail()
+    .custom(async (email) => {
+      const user = await UserService.findByEmail(email);
+      if (user) {
+        throw new Error('Email in use');
+      }
+    }),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*/)
+    .withMessage(
+      'Password have at least 1 uppercase, 1 lower case and 1 number'
+    ),
+  async (req, res) => {
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      // const response = {
+      //   validationErrors: { ...req.validationErrors },
+      // };
+      const validationErrors = {};
+      result.errors.forEach(
+        (error) => (validationErrors[error.path] = error.msg)
+      );
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
+
+    await UserService.save(req.body);
+    return res.send({ message: 'User created' });
+  }
+);
 
 module.exports = router;
